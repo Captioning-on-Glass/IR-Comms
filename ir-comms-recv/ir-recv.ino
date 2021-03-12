e70a8f73e5f5ec58181c71a8b987bcd6d7d314f8
Gabriel <quontas@gmail.com>
2021-03-12 15:11:39 -0500

    Serial -> SerialUSB

--- ir-comms-recv/ir-recv.ino
+++ ir-comms-recv/ir-recv.ino
@@ -1,25 +1,25 @@
-// #include <IRremote.h>
+#include <IRremote.h>
 
-// const uint8_t RECEIVER_PIN = 7;
-// IRrecv irrecv(RECEIVER_PIN);
-// decode_results results;
+const uint8_t RECEIVER_PIN = 7;
+IRrecv irrecv(RECEIVER_PIN);
+decode_results results;
 
 void setup()
 {
-    // pinMode(RECEIVER_PIN, INPUT);
-    Serial.begin(9600);
-    Serial.println("Is this thing on?");
-    // irrecv.enableIRIn();
+    pinMode(RECEIVER_PIN, INPUT);
+    SerialUSB.begin(9600);
+    SerialUSB.println("Is this thing on?");
+    irrecv.enableIRIn();
 }
 
 void loop()
 {
-    Serial.println("Loop running");
-    // if (irrecv.decode(&results))
-    // {
-    //     Serial.print("Received a value!! It is ");
-    //     Serial.println(results.value, HEX);
-    //     irrecv.resume();
-    //     delay(150);
-    // }
+    // SerialUSB.println("Loop running");
+    if (irrecv.decode(&results))
+    {
+        SerialUSB.print("Received a value!! It is ");
+        SerialUSB.println(results.value, HEX);
+        irrecv.resume();
+        delay(150);
+    }
 }
