5f60da847d3190c1802944d8fbdd502ed73d02cc
Gabriel <quontas@gmail.com>
2021-04-27 14:13:39 -0400

    Command and address updated.

--- ir-comms-send/ir-transmit.ino
+++ ir-comms-send/ir-transmit.ino
@@ -5,9 +5,13 @@
 #define Serial SerialUSB
 #endif
 
-#define IR_SEND_PIN 4
+#define IR_SEND_PIN A2
+#define ADDRESS 0x0
+#define COMMAND 0x40
+#define REPEATS 0
 
-void setup() {
+void setup()
+{
     pinMode(LED_BUILTIN, OUTPUT);
 
     Serial.begin(115200);
@@ -24,51 +28,25 @@ void setup() {
     Serial.println(IR_SEND_PIN);
 }
 
-/*
- * Set up the data to be sent.
- * For most protocols, the data is build up with a constant 8 (or 16 byte) address
- * and a variable 8 bit command.
- * There are exceptions like Sony and Denon, which have 5 bit address.
- */
-uint16_t sAddress = 0x0102;
-uint8_t sCommand = 0x34;
-uint8_t sRepeats = 0;
-
-void loop() {
+void loop()
+{
     /*
      * Print current send values
      */
     Serial.println();
     Serial.print(F("Send now: address=0x"));
-    Serial.print(sAddress, HEX);
+    Serial.print(ADDRESS, HEX);
     Serial.print(F(" command=0x"));
-    Serial.print(sCommand, HEX);
+    Serial.print(COMMAND, HEX);
     Serial.print(F(" repeats="));
-    Serial.print(sRepeats);
+    Serial.print(REPEATS);
     Serial.println();
 
     Serial.println(F("Send NEC with 16 bit address"));
     Serial.flush();
 
     // Results for the first loop to: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
-    IrSender.sendNEC(sAddress, sCommand, sRepeats);
+    IrSender.sendNEC(ADDRESS, COMMAND, REPEATS);
 
-    /*
-     * If you cannot avoid to send a raw value directly like e.g. 0xCB340102 you must use sendNECRaw()
-     */
-//    Serial.println(F("Send NECRaw 0xCB340102"));
-//    IrSender.sendNECRaw(0xCB340102, sRepeats);
-    /*
-     * Increment send values
-     * Also increment address just for demonstration, which normally makes no sense
-     */
-    sAddress += 0x0101;
-    sCommand += 0x11;
-    sRepeats++;
-    // clip repeats at 4
-    if (sRepeats > 4) {
-        sRepeats = 4;
-    }
-
-    delay(1000);  // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
+    delay(1000); // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
 }
